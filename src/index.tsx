ffadb3dc7a6a9a5ed40e8b25ef58b9414c35ac3b
Rico <richardl@hancock1.com>
2023-07-19 14:20:43 -0600

    Init

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+// Delete me
+export const Thing = () => {
+  return (
+    <div className="bg-blue-500 p-9">
+      the snozzberries taste like snozzberries
+    </div>
+  );
+};
