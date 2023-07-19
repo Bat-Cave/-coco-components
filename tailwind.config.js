3537f1ac75dbe38c717f7e909cabbd1d69e2d1e9
Rico <richardl@hancock1.com>
2023-07-19 14:36:13 -0600

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './stories/*'],
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './stories/*'],
   theme: {
     extend: {},
   },
